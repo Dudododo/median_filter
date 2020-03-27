37296eb51d56cee68162eeea2ad841ba99497dbe
Dudododo <a752601260@gmail.com>
2020-03-27 14:14:21 +0800

    添加项目文件。

---
+++ median_filter.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture cap;
+
+	cap.open(0);
+
+	while (1)
+	{
+		cv::Mat src;
+		cv::Mat dst;
+
+		cap >> src;
+		cv::medianBlur(src, dst, 51); //中值滤波
+
+		cv::imshow("原图", src);
+		cv::imshow("中值滤波", dst);
+
+		waitKey(30);
+	}
+}
+
+
